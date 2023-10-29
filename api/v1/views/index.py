cd4628b683dff3f85e3c7870fa692cf1bfbc2385
Polaris-algedi <karchalmohammed@gmail.com>
2023-10-29 12:46:00 -0700

    Update index.py by create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ This module defines a route status for a Flask application using a Blueprint.
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -13,3 +14,20 @@ def hbnb_status():
     It returns a JSON object with the status of the API.
     """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnb_stats():
+    """Stats"""
+    hbnb_objs = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    stats = {}
+    for key, value in hbnb_objs.items():
+        stats[key] = storage.count(value)
+    return jsonify(stats)
